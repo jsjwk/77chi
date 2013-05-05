6bd6d2769acf69a4ffe5596bf75d40f830014f68
jsjwk <jsjwk@126.com>
2013-05-05 23:52:15 +0800

    update

---
+++ userweb/src/main/java/com/chi/controller/LoginController.java
@@ -0,0 +1,27 @@
+package com.chi.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.taobao.api.ApiException;
+
+public class LoginController extends UserBaseController {
+
+    /**
+     * 用户登录
+     */
+    @RequestMapping("/login.do")
+    public String login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ApiException
+    {
+    	
+    	
+    	
+    	return null;
+    }
+    
+}
