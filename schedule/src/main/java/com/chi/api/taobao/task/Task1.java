14ee23331d321ccf37257bd3f4eda71509d825d6
jsjwk <jsjwk@126.com>
2013-05-14 22:58:06 +0800

    update

--- schedule/src/main/java/com/chi/api/taobao/task/Task1.java
+++ schedule/src/main/java/com/chi/api/taobao/task/Task1.java
@@ -21,59 +21,89 @@ import com.taobao.api.domain.TaobaokeItemDetail;
 
 public class Task1 {
 
-    private static ApplicationContext context = new ClassPathXmlApplicationContext("classpath:/spring-schedule.xml");
-    private static TaobaoApiService taobaoApiService = (TaobaoApiService) context.getBean("taobaoApiService");
-    private static TaobaokeItemDao taobaokeItemDao = (TaobaokeItemDao) context.getBean("taobaokeItemDao");
-    private static TaobaokeItemDetailDao taobaokeItemDetailDao = (TaobaokeItemDetailDao) context.getBean("taobaokeItemDetailDao");
+	private static ApplicationContext context = new ClassPathXmlApplicationContext("classpath:/spring-schedule.xml");
+	private static TaobaoApiService taobaoApiService = (TaobaoApiService) context.getBean("taobaoApiService");
+	private static TaobaokeItemDao taobaokeItemDao = (TaobaokeItemDao) context.getBean("taobaokeItemDao");
+	private static TaobaokeItemDetailDao taobaokeItemDetailDao = (TaobaokeItemDetailDao) context.getBean("taobaokeItemDetailDao");
 
-    private static final Logger LOG = LoggerFactory.getLogger(Task1.class);
+	private static final Logger LOG = LoggerFactory.getLogger(Task1.class);
 
-    public static void main(String[] args) throws ApiException
-    {
-	Date now = new Date();
-	Long cid = 50026316L;
-	// 获取淘宝客商品：饮料
-	List<TaobaokeItem> listTaobaokeItem = taobaoApiService.getTaobaokeItems(cid, 1L, 40L);
-	List<Long> numIidsList = new ArrayList<Long>();
-	for (TaobaokeItem taobaokeItem : listTaobaokeItem)
+	public static void main(String[] args) throws ApiException 
 	{
-	    TaobaokeItemVo vo = new TaobaokeItemVo(taobaokeItem);
-	    Long numIid = taobaokeItem.getNumIid();
-	    vo.setNumIid(numIid);
-	    vo.setCid(cid);
-	    vo.setCreateTime(now);
-	    vo.setUpdateTime(now);
-	    vo.setOverseasItem("false");
-	    // 存储商品基本信息
-	    taobaokeItemDao.insertTaobaokeItemVo(vo);
-	    numIidsList.add(taobaokeItem.getNumIid());
+		//获取[保健营养]并且保存
+		getItemsAndStore(50020275L);
+		getItemsAndStore(50026800L);
+		
+		//获取[进口食品]并且保存
+		
+		
+		//获取[酒类]并且保存
+		getItemsAndStore(50008141L);
+		
+		//获取[母婴食品]并且保存
+		getItemsAndStore(35L);
+		getItemsAndStore(50022517L);
+		
+		//获取[水果]并且保存
+		getItemsAndStore(50050725L);
+		
+		//获取[休闲零食]并且保存
+		getItemsAndStore(50002766L);
+		
+		//获取[饮料]并且保存
+		getItemsAndStore(50026316L);
+		
 	}
 
-	List<TaobaokeItemDetailVo> listTaobaokeItemDetailVo = new ArrayList<TaobaokeItemDetailVo>();
-	StringBuilder numIidBuilder = new StringBuilder();
-	for (int i = 1; i <= numIidsList.size(); i++)
+	/**
+	 * 获取某个类别下面的商品&保存
+	 * @param cid
+	 * @throws ApiException
+	 */
+	private static void getItemsAndStore(Long cid) throws ApiException 
 	{
-	    numIidBuilder.append(numIidsList.get(i - 1)).append(",");
-	    if (i % 10 == 0 || i == numIidsList.size())
-	    {// 每十条执行一次详细信息查询
-	     // 获取商品详细信息
-		List<TaobaokeItemDetail> listTaobaokeItemDetail = taobaoApiService.getTaobaokeItemDetail(numIidBuilder.deleteCharAt(numIidBuilder.length() - 1).toString());
-		for (TaobaokeItemDetail taobaokeItemDetail : listTaobaokeItemDetail)
+		Date now = new Date();
+		// 获取淘宝客商品
+		List<TaobaokeItem> listTaobaokeItem = taobaoApiService.getTaobaokeItems(cid, 1L, 40L);
+		List<Long> numIidsList = new ArrayList<Long>();
+		for (TaobaokeItem taobaokeItem : listTaobaokeItem) 
 		{
-		    TaobaokeItemDetailVo taobaokeItemDetailVo = new TaobaokeItemDetailVo(taobaokeItemDetail);
-		    //taobaokeItemDetailVo.setNumIid(taobaokeItemDetail.getItem().getNumIid());
-		    taobaokeItemDetailVo.setNumIid(UUID.randomUUID().getLeastSignificantBits());
-		    taobaokeItemDetailVo.setCreateTime(now);
-		    taobaokeItemDetailVo.setUpdateTime(now);
-		    listTaobaokeItemDetailVo.add(taobaokeItemDetailVo);
+			TaobaokeItemVo vo = new TaobaokeItemVo(taobaokeItem);
+			Long numIid = taobaokeItem.getNumIid();
+			vo.setNumIid(numIid);
+			vo.setCid(cid);
+			vo.setCreateTime(now);
+			vo.setUpdateTime(now);
+			vo.setOverseasItem("false");
+			// 存储商品基本信息
+			taobaokeItemDao.insertTaobaokeItemVo(vo);
+			numIidsList.add(taobaokeItem.getNumIid());
 		}
-		numIidBuilder.delete(0, numIidBuilder.length() - 1);
-	    }
+
+		List<TaobaokeItemDetailVo> listTaobaokeItemDetailVo = new ArrayList<TaobaokeItemDetailVo>();
+		StringBuilder numIidBuilder = new StringBuilder();
+		for (int i = 1; i <= numIidsList.size(); i++) 
+		{
+			numIidBuilder.append(numIidsList.get(i - 1)).append(",");
+			if (i % 10 == 0 || i == numIidsList.size()) 
+			{// 每十条执行一次详细信息查询
+				// 获取商品详细信息
+				List<TaobaokeItemDetail> listTaobaokeItemDetail = taobaoApiService.getTaobaokeItemDetail(numIidBuilder.deleteCharAt(numIidBuilder.length() - 1).toString());
+				for (TaobaokeItemDetail taobaokeItemDetail : listTaobaokeItemDetail) 
+				{
+					TaobaokeItemDetailVo taobaokeItemDetailVo = new TaobaokeItemDetailVo(taobaokeItemDetail);
+					// taobaokeItemDetailVo.setNumIid(taobaokeItemDetail.getItem().getNumIid());
+					taobaokeItemDetailVo.setNumIid(UUID.randomUUID().getLeastSignificantBits());
+					taobaokeItemDetailVo.setCreateTime(now);
+					taobaokeItemDetailVo.setUpdateTime(now);
+					listTaobaokeItemDetailVo.add(taobaokeItemDetailVo);
+				}
+				numIidBuilder.delete(0, numIidBuilder.length() - 1);
+			}
+		}
+
+		// 存储详细信息
+		taobaokeItemDetailDao.batchInsertTaobaokeItemDetailVo(listTaobaokeItemDetailVo);
 	}
-
-	// 存储详细信息
-	taobaokeItemDetailDao.batchInsertTaobaokeItemDetailVo(listTaobaokeItemDetailVo);
-
-    }
 
 }
--- userweb/src/main/java/com/chi/controller/RegisterController.java
+++ userweb/src/main/java/com/chi/controller/RegisterController.java
@@ -1,18 +1,21 @@
 package com.chi.controller;
 
 import java.io.IOException;
+import java.util.Date;
 
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.chi.constant.ConstatVar;
-import com.qq.connect.QQConnectException;
+import com.chi.po.UserInfo;
+import com.chi.service.UserInfoService;
 import com.taobao.api.ApiException;
 
 /**
@@ -24,7 +27,34 @@ import com.taobao.api.ApiException;
 @Controller(value = "registerController")
 public class RegisterController extends UserBaseController {
 
+	@Resource(name="userInfoService")
+	private UserInfoService userInfoService;
 	private static final Logger LOG = LoggerFactory.getLogger(RegisterController.class);
 
+    /**
+     * 用户登录
+     */
+    @RequestMapping("/reg.do")
+    public String register(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ApiException
+    {
+    	String email = request.getParameter("email");
+    	String password = request.getParameter("password");
+    	String repassword = request.getParameter("repassword");
+    	if(StringUtils.isEmpty(email) || StringUtils.isEmpty(password)
+    			|| !password.equals(repassword))
+    	{
+    		return "error";
+    	}
+    	
+    	UserInfo userInfo = new UserInfo();
+    	userInfo.setAccountType(UserInfo.ACCOUNTTYPE_DEFAULT);
+    	userInfo.setCreateTime(new Date());
+    	userInfo.setEmail(email);
+    	userInfo.setNickName(email.substring(0, email.indexOf("@")));
+    	userInfo.setSourceType(UserInfo.SOURCETYPE_REGISTER);
+    	userInfo.setPassword(password);
+    	userInfoService.saveOrUpdateUserInfo(userInfo);
+    	return "index";
+    }
 
 }
