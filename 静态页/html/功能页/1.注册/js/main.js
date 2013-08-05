87d989a6f7cbdeb04f84fa234b6ff2b938807da5
test <test@jiaoluoyi>
2013-08-05 17:42:49 +0800

    1

--- 静态页/html/功能页/1.注册/js/main.js
+++ 静态页/html/功能页/1.注册/js/main.js
@@ -1,6 +1,6 @@
 ﻿$(function(){
 //垂直居中
 var body_hight = document.documentElement.clientHeight;
-var margin_top = (body_hight - $(".reg_md").height())/2 + "px";
-$(".reg_md").css("margin-top",margin_top);
+var margin_top = (body_hight - $(".md").height())/2 + "px";
+$(".md").css("margin-top",margin_top);
 })
