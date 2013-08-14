870a42d83908f8718d4a80817c2ca594de3e9a72
test <test@jiaoluoyi>
2013-08-14 17:33:39 +0800

    a

--- 静态页/html/功能页/1.注册/js/main.js
+++ 静态页/html/功能页/1.注册/js/main.js
@@ -4,6 +4,9 @@ middle();
 
 /*帐号密码清空*/
 empty_input();
+
+/*初始化输入框*/
+init_input();
 })
 
 /*垂直居中*/
@@ -15,17 +18,34 @@ $(".md").css("margin-top",margin_top);
 
 /*帐号密码清空*/
 function empty_input(){
-var d = $("#username, #password");
+var d = $("#un, #ps");
 d.focusin(function(i) {
 	$(this).siblings("i").css("display", "none");
 	/*更换背景*/
-	$(this).css("background-position","0px -66x");
+	$(this).parent(".i").css("background-position","0px -66px");
 });
 d.focusout(function(i) {
-	if ($.trim(this.value) == "") {
-		$(this).siblings("i").css("display", "block");
-		/*更换背景*/
-		$(this).css("background-position","0px -27x");
-	}
+	/*if ($.trim(this.value) == "") {
+		$(this).siblings("i").css("display", "block");	
+	}*/
+	change_dis(this.value);
+	/*更换背景*/
+	$(this).parent(".i").css("background-position","0px -27px");
 });
+}
+
+/*初始化输入框*/
+function init_input(){
+	var v = $("#un").val();
+	change_dis(v);
+	v = $("#pw").val();
+	change_dis(v);
+}
+
+function change_dis(v){
+	if($.trim(v)==""){
+		$(this).siblings("i").css("display", "block");	
+	}else{
+		$(this).siblings("i").css("display", "none");	
+	}
 }
